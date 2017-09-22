74fac9e3be263e2c1efa28a0cf90af194740928d
chan135 <chan135@purdue.edu>
2017-09-21 21:57:20 -0400

    Frontend for question

---
+++ app.js
@@ -0,0 +1,22 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        question: '',
+        answer: '',
+        answer1: '',
+        answer2: '',
+        answer3: '',
+        answer4: '',
+        answer5: '',
+        tf:true,
+        mc:false
+    },
+    methods: {
+        tf:function() {
+
+        },
+        mc:function() {
+
+        }
+    }
+});
