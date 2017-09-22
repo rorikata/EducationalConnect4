715c5eefcb3c39297ad32ffbf6c8e6b86e461796
chan135 <chan135@purdue.edu>
2017-09-21 21:58:31 -0400

    frontend for profile

---
+++ app_profile.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        name: '',
+        age: ''
+    },
+    methods: {
+        add:function() {
+          this.age++
+        },
+        sub:function() {
+          this.age--
+        }
+    }
+});
