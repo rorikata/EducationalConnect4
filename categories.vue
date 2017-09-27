f4c2246befcd2103daf202abf8f12f248700f3d5
chan135 <chan135@purdue.edu>
2017-09-27 16:15:43 -0400

    Close to filtering

--- categories.vue
+++ categories.vue
@@ -3,12 +3,17 @@
     <form>
       <div class="well">
         <h4>Categories</h4>
-        <select >
-          <option v-for="category in categories">{{category.color}} </option>
-
+        <select v-model="catNum">
+          <option v-for="category in categories" v-bind:value="category.num">{{category.name}} </option>
         </select>
+        <p>{{catNum}}</p>
       </div>
-      <button type="submit" class="btn btn-large btn-block btn-primary full-width" v-on:click="addC">Submit</button>
+      <select v-model="subCatNum">
+        <option v-for="subcategory in filteredSubCats">{{subcategory.name}} </option>
+      </select>
+      <p>{{subCatNum}}</p>
+
+      <button type="submit" class="btn btn-large btn-block btn-primary full-width" >Submit</button>
       <button class="btn btn-large btn-block btn-success full-width">Go User</button> </br></br>
 
     </form>
@@ -17,30 +22,112 @@
 
 <script>
 
+import axios from 'axios';
 export default {
   data () {
     return {
+      catNum: '',
+      subCatNum: '',
       categories: [
-        {color:'red'},
-        {color:'yellow'},
-        {color:'blue'}
+        {name: 'cs', num: 0},
+        {name: 'eng', num: 1},
+        {name: 'math', num: 2}
       ],
-      newC: ''
+      subcategories: [
+        {name: 'compilers', numP: 0, num: 0},
+        {name: '408', numP: 0, num: 1},
+        {name: 'Writing', numP: 1, num: 0},
+        {name: 'Lit', numP: 1, num: 1},
+        {name: 'Calc 2', numP: 2, num: 0},
+        {name: 'Linear', numP: 2, num: 1}
+      ],
+      questions: [
+        {question: 'compilers', answer_type: 'mc', multiple_choice: [
+          {fake1: 'yello'},
+          {fake2: 'rello'},
+          {fake3: 'mello'},
+          {ans: 'u rite'}
+        ], true_false: '', category: 0.0},
+        {question: 'compilers2', answer_type:'tf', multiple_choice: [
+          {fake1:''},
+          {fake2:''},
+          {fake3:''},
+          {ans:''}
+        ], true_false: 'T', category: 0.0},
+        {question: '408', answer_type:'tf', multiple_choice: [
+          {fake1:''},
+          {fake2:''},
+          {fake3:''},
+          {ans:''}
+        ], true_false: 'T', category: 0.1},
+        {question: '4082', answer_type:'tf', multiple_choice: [
+          {fake1:''},
+          {fake2:''},
+          {fake3:''},
+          {ans:''}
+        ], true_false: 'T', category: 0.1},
+        {question: 'Writing', answer_type:'tf', multiple_choice: [
+          {fake1:''},
+          {fake2:''},
+          {fake3:''},
+          {ans:''}
+        ], true_false: 'T', category: 1.0},
+        {question: 'Writing2', answer_type:'tf', multiple_choice: [
+          {fake1:''},
+          {fake2:''},
+          {fake3:''},
+          {ans:''}
+        ], true_false: 'T', category: 1.0},
+        {question: 'Lit', answer_type:'tf', multiple_choice: [
+          {fake1:''},
+          {fake2:''},
+          {fake3:''},
+          {ans:''}
+        ], true_false: 'T', category: 1.1},
+        {question: 'Lit2', answer_type:'tf', multiple_choice: [
+          {fake1:''},
+          {fake2:''},
+          {fake3:''},
+          {ans:''}
+        ], true_false: 'T', category: 1.1},
+        {question: 'Calc', answer_type:'tf', multiple_choice: [
+          {fake1:''},
+          {fake2:''},
+          {fake3:''},
+          {ans:''}
+        ], true_false: 'T', category: 2.0},
+        {question: 'Calc2', answer_type:'tf', multiple_choice: [
+          {fake1:''},
+          {fake2:''},
+          {fake3:''},
+          {ans:''}
+        ], true_false: 'T', category: 2.0},
+        {question: 'Linear', answer_type:'tf', multiple_choice: [
+          {fake1:''},
+          {fake2:''},
+          {fake3:''},
+          {ans:''}
+        ], true_false: 'T', category: 2.1},
+        {question: 'Linear2', answer_type:'tf', multiple_choice: [
+          {fake1:''},
+          {fake2:''},
+          {fake3:''},
+          {ans:''}
+        ], true_false: 'T', category: 2.1},
+
+      ]
     }
   },
   methods: {
-    addC () {
-      let newCat = {
-        newC: this.newC
-      }
-      console.log(newC)
-      axios.post('http://localhost:3000/users', newC)
-        .then((response) => {
-          console.log(response)
-        })
-        .catch((error) => {
-          console.log(error)
-        })
+
+
+  },
+  computed: {
+    filteredSubCats: function() {
+      return this.subcategories.filter((subcategory) => {
+        return subcategory.title.match(this.catNum);
+      });
+
     }
   }
 }
