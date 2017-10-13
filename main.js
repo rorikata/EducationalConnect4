67377473342dfef97693f4c6db25c8de9cf4af47
chan135 <chan135@purdue.edu>
2017-10-13 17:54:33 -0400

    added bootstrap

--- main.js
+++ main.js
@@ -5,7 +5,13 @@ import App from './App'
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import Routes from './routes'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 
+// Import the styles directly. (Or you could add them via script tags.)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
